70a559e1758e04b33b0140cb0e4beb711269ec3a
Tyler George <TGeorge@cornellcollege.edu>
2025-08-04 23:11:42 -0500

    add JSM talk
    created scrollytelling talk for JSM

--- Readme.md
+++ Readme.md
@@ -11,6 +11,9 @@ This repository contains talks and posters on the course *The History of Statist
 
 [Slides](https://stats-tgeorge.github.io/STA257_talks/Gustavus#) for a 60 minute presentation, at Gustavus Adolphus College on April 14th, 2025. 
 
-## Poster at USCOTS
+## Poster at USCOTS 2025
 
 [Poster](https://stats-tgeorge.github.io/STA257_talks/docs/Poster_USCOTS25.pdf) printed for USCOTS Conference on Friday, July 18th from 11:15 am – 12:30 pm CT.
+
+## Talk at JSM 2025
+
---
+++ _temp_pic_code.R
@@ -0,0 +1,68 @@
+# FUN
+
+
+## Pictures
+
+![](imgs/photos/b_s_pump.JPEG)
+
+## Pictures
+
+![](imgs/photos/gia_caus.JPEG)
+
+## Pictures
+
+![](imgs/photos/IMG_3923.JPEG)
+
+## Pictures
+
+![](imgs/photos/nat_arch.JPEG)
+
+## Pictures
+
+![](imgs/photos/titanic_2.JPEG)
+
+
+
+## Pictures
+
+![](imgs/photos/tea_bus.png)
+
+## Pictures
+
+![](imgs/photos/giants_causeway.png)
+
+## Pictures
+
+![](imgs/photos/class_pumpkin.jpg)
+
+## Pictures
+
+![](imgs/photos/titanic_flying.png)
+
+
+## Pictures
+
+![](imgs/photos/class_buck_palace.png)
+
+
+## Pictures
+
+![](imgs/photos/football.png)
+
+## Pictures
+
+![](imgs/photos/gelato.png)
+
+## Pictures
+
+![](imgs/photos/rothamsted_bench.png)
+
+## Pictures
+
+![](imgs/photos/arthurs_seat.png)
+
+
+## Pictures
+
+![](imgs/photos/coffee.JPEG)
+
